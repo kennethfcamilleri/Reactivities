7a8685911ea724a9d9ca22694920e753014e6049
Kenneth Forseth Camilleri <kenneth.forseth@gmail.com>
2021-09-12 11:38:19 +0200

    Added Client Side Attendance Feature

--- client-app/src/api/agent.ts
+++ client-app/src/api/agent.ts
@@ -1,6 +1,6 @@
 import { User, UserFormValues } from './../app/models/user';
 import { store } from './../app/stores/store';
-import { Activity } from './../app/models/activity';
+import { Activity, ActivityFormValues } from './../app/models/activity';
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import { toast } from 'react-toastify';
 import { history } from '..';
@@ -71,9 +71,10 @@ const requests = {
 const Activities = {
     list: () => requests.get<Activity[]>('/activities'),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => axios.post<void>('/activities', activity),
-    update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
-    delete: (id: string) => axios.delete<void>(`/activities/${id}`)
+    create: (activity: ActivityFormValues) => requests.post<void>('/activities', activity),
+    update: (activity: ActivityFormValues) => requests.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.del<void>(`/activities/${id}`),
+    attend: (id: string) => requests.post<void>(`/activities/${id}/attend`, {})
 }
 
 const Account = {
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
 import { Button, Header, Segment } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
-import { Activity } from "../../../app/models/activity";
+import { Activity, ActivityFormValues } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/store";
 import { v4 as uuid } from 'uuid';
 import { Formik, Form } from "formik";
@@ -20,15 +20,7 @@ export default observer(function ActivityForm() {
     const {createActivity, updateActivity, loading, loadActivity, loadingInitial} = activityStore;
     const {id} = useParams<{id: string}>();
 
-    const [activity, setActivity] = useState<Activity>({
-        id: '',
-        title: '',
-        category: '',
-        description: '',
-        date: null,
-        city: '',
-        venue: ''
-    });
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required'),
@@ -40,11 +32,11 @@ export default observer(function ActivityForm() {
     })
 
     useEffect(() => {
-        if (id) loadActivity(id).then(activity => setActivity(activity!))
+        if (id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)))
     }, [id, loadActivity]);
 
-    function handleFormSubmit(activity: Activity) {
-        if (activity.id.length === 0) {
+    function handleFormSubmit(activity: ActivityFormValues) {
+        if (!activity.id) {
             let newActivity = {
                 ...activity, 
                 id: uuid()
@@ -82,7 +74,7 @@ export default observer(function ActivityForm() {
                     <MyTextInput placeholder='Venue' name='venue' />
                     <Button 
                         disabled={isSubmitting || !dirty || !isValid}
-                        loading={loading} floated='right' 
+                        loading={isSubmitting} floated='right' 
                         positive type='submit' content='Submit' />
                     <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' />
                 </Form>
